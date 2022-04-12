1f8b23d2a4e81916b8e87424fec0d739a0786288
hikaru <alclimb@users.noreply.github.com>
2022-04-12 10:54:51 +0900

    feat: JPEG圧縮処理を追加

--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
-console.log(`main >>`);
+import * as Sharp from "sharp";
+
+// async function main() {
+//   console.log(`main >>`);
+
+//   let buffer = await Sharp(`./assets/src.jpg`).toBuffer();
+
+//   for (let i = 0; i < 2; i++) {
+//     buffer = await compression(buffer);
+//   }
+
+//   await Sharp(buffer).toFile(`out.jpg`);
+// }
+
+// async function compression(src: Buffer) {
+//   const dst = await Sharp(src)
+//     .jpeg({ quality: 85 })
+//     .toBuffer();
+
+//   return dst;
+// }
+
+
+async function main() {
+  console.log(`main >>`);
+
+  const sharp = Sharp(`./assets/src.jpg`);
+
+  for (let i = 0; i < 10; i++) {
+    // 
+    sharp.jpeg({ quality: 85 });
+
+    console.log(`  i="${i}"`);
+  }
+
+  sharp.toFile(`out.jpg`);
+}
+
+main();
