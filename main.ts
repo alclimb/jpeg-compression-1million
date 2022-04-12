317a429fe784743ddacd23c5b5bc1ea66d2f909d
hikaru <alclimb@users.noreply.github.com>
2022-04-12 13:08:40 +0900

    feat: 変換クオリティとサイズを変更

--- main.ts
+++ main.ts
@@ -1,39 +1,41 @@
 import * as Sharp from "sharp";
 
-// async function main() {
-//   console.log(`main >>`);
-
-//   let buffer = await Sharp(`./assets/src.jpg`).toBuffer();
-
-//   for (let i = 0; i < 2; i++) {
-//     buffer = await compression(buffer);
-//   }
-
-//   await Sharp(buffer).toFile(`out.jpg`);
-// }
-
-// async function compression(src: Buffer) {
-//   const dst = await Sharp(src)
-//     .jpeg({ quality: 85 })
-//     .toBuffer();
-
-//   return dst;
-// }
-
-
 async function main() {
   console.log(`main >>`);
 
-  const sharp = Sharp(`./assets/src.jpg`);
+  let buffer = await Sharp(`./assets/src.jpg`).toBuffer();
 
-  for (let i = 0; i < 10; i++) {
-    // 
-    sharp.jpeg({ quality: 85 });
+  for (let i = 0; i < 1000000; i++) {
+    buffer = await compression(buffer);
 
     console.log(`  i="${i}"`);
   }
 
-  sharp.toFile(`out.jpg`);
+  await Sharp(buffer).toFile(`out.jpg`);
 }
+
+async function compression(src: Buffer) {
+  const dst = await Sharp(src)
+    .jpeg({ quality: 30 })
+    .toBuffer();
+
+  return dst;
+}
+
+
+// async function main() {
+//   console.log(`main >>`);
+
+//   const sharp = Sharp(`./assets/src.jpg`);
+
+//   for (let i = 0; i < 1000000; i++) {
+//     // 
+//     sharp.jpeg({ quality: 30 });
+
+//     console.log(`  i="${i}"`);
+//   }
+
+//   sharp.toFile(`out.jpg`);
+// }
 
 main();
